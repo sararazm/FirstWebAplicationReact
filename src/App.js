d54f701db75a62bfc5f4735b591bfa5f981d03d8
Sara Razm <razmsara@gmail.com>
2022-03-07 15:28:19 +0100

    All pages routed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 import Home from './components/Pages/home/Home';
 import About from './components/Pages/about/About';
-import Header from './components/header/Header';
 import Contact from './components/Pages/contact/Contact';
 import Footer from './components/footer/Footer';
+import Header from '../src/components/Pages/Header';
+import NoPage from '../src/components/Pages/NoPage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
 
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
+
       <Header />
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
       <Footer />
-    </div>
+
+    </BrowserRouter>
   );
 }
 
